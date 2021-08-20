9763b3bf9a51db9003cdbac7055dd8f521cf91a7
Yuki Hashimoto <ykmhtshauoioi1104@gmail.com>
2021-08-21 00:05:10 +0900

    IME: Text Object window reposition

--- source/blender/editors/curve/editfont.c
+++ source/blender/editors/curve/editfont.c
@@ -1665,6 +1665,25 @@ static int insert_text_exec(bContext *C, wmOperator *op)
 }
 
 #ifdef WITH_INPUT_IME
+static void font_ime_reposition(wmWindow *win, ARegion *region, Object *obedit, const float textcurs[2])
+{
+  float projmat[4][4];
+  float co[3];
+  float r_co[2];
+
+  copy_v2_v2(co, textcurs);
+  co[2] = 0.0;
+  ED_view3d_ob_project_mat_get(region->regiondata, obedit, projmat);
+  ED_view3d_project_float_v2_m4(region, co, r_co, projmat);
+
+  wm_window_IME_begin(win,
+                      (int)r_co[0] + region->winrct.xmin,
+                      (int)r_co[1] + region->winrct.ymin,
+                      0,
+                      0,
+                      false);
+}
+
 static int insert_text_ime_invoke(bContext *C, wmOperator *op, const int ime_event_type)
 {
   wmWindow *win = CTX_wm_window(C);
@@ -1693,6 +1712,8 @@ static int insert_text_ime_invoke(bContext *C, wmOperator *op, const int ime_eve
         cu->curinfo.flag &= (~CU_CHINFO_IME_COMPOSITE);
 
         RNA_string_set(op->ptr, "text", ime_data->str_composite);
+
+        font_ime_reposition(win, CTX_wm_region(C), obedit, ef->textcurs[0]);
 
         return OPERATOR_FINISHED;
       }
